07f443a8ec24ac0770f0b1ac5c67f7dcc4faa3ac
kechit sonia <kechitsonia@MacBook-Air-de-kechit.local>
2022-02-24 19:20:38 +0100

    commentaires

--- assets/js/scripts/factories/photographers_profil.js
+++ assets/js/scripts/factories/photographers_profil.js
@@ -1,117 +1,160 @@
 // page individuelle
+function mediaFactory(media, photographer) {
 
+    const {image, title, likes, photographerId, video, id} = media;
+    const [{name, portrait, city, country, tagline}] = photographer;
 
-
-
-function mediaFactory(data) {
-    const {image, title, likes, photographerId} = data;
-    // console.log(data.photographerId);
-
-    const pictures = `/assets/images/photos/Mimi/${image}`;
+    const pictures = `/assets/images/photos/${name}/${image}`;
+    const videos = `/assets/images/photos/${name}/${video}`;
+    const portraitHeader = `/assets/images/photos/Photographers_ID_Photos/${portrait}`;
+    const likeImg = `/assets/images/like.png`;
 
     function getMediaCardDOM() {
         // création des élements
-        const section = document.createElement( 'section' );
-        const img = document.createElement( 'img' );
-        const titre = document.createElement( 'p' );
-        const jaime= document.createElement( 'p' );
+        const imgHeader = document.getElementById("photographer-header");
+        const h2_modal = document.getElementById( 'h2-modal' );
+        // ----------------
+        const medias = document.getElementById('liste-medias');
+        const h1 = document.getElementById( "titre-header" );
+        const ville__pays = document.getElementById( 'ville-pays_header' );
+        const proverbe = document.getElementById( 'proverbe-header' );
+        // const headerPhtotograph = `<div>
+        //                                 <h1 id="titre-header">${name}</h1>
+        //                                 <h2 id="ville-pays_header">${city + ", " + country}</h2>
+        //                                 <p id="proverbe-header">${tagline}</p>
+        //                            </div>`
+        // const section = `<section id=${photographerId} class='media'>
+        //                     <img src="${pictures}"" id='images-medias' onclick='displayModalPicture()' onerror="this.style.display='none'"></img>
+        //                     <video src="${videos}" controls='controls' onerror="this.style.display='none'"></video>
+        //                     <div class='description-photos'>
+        //                         <p>${title}</p>
+        //                         <p class='jaime-photo' data-jaime="${id}">${likes}</p>
+        //                         <img src="${likeImg}" class='likes-img' data-id="${id}"></img>
+        //                     </div>
+        //                 </section>`
+        medias.innerHTML = `<section id=${photographerId} class='media'>
+                                ${photographer.map((photographer, index) =>
+                                    )}
+                                <img src="${pictures}" id='images-medias' onclick='displayModalPicture()' onerror="this.style.display='none'"></img>
+                                <video src="${videos}" controls='controls' onerror="this.style.display='none'"></video>
+                                <div class='description-photos'>
+                                    <p>${title}</p>
+                                    <p class='jaime-photo' data-jaime="${id}">${likes}</p>
+                                    <img src="${likeImg}" class='likes-img' data-id="${id}"></img>
+                                </div>
+                            </section>`
+        // const section = document.createElement( 'section' );
+        // const img = document.createElement( 'img' );
+        // const vid = document.createElement( 'video');
+        // const div = document.createElement( 'div' );
+        // const titre = document.createElement( 'p' );
+        // const jaime= document.createElement( 'p' );
+        // const test = document.createElement( 'img' );
+        // const prix_photographe = document.getElementById("prix-photographe");
+
         // ajout d'un attribut
-        img.setAttribute("src", pictures);
-        section.classList.add("media_profil");
-        section.setAttribute("id", photographerId);
+        imgHeader.setAttribute("src", portraitHeader);
+        imgHeader.classList.add("photographer-header");
+        // img.setAttribute("id", "images-medias");
+        // section.setAttribute("id", photographerId);
+        // img.setAttribute("src", pictures);
+        // test.setAttribute("src", likeImg);
+        // vid.setAttribute("src", videos);
+        // img.setAttribute("onclick", "displayModalPicture()");
+        // vid.setAttribute("controls", "controls");
+        // Fonction : Si une balise image ou une balise vidéo est vide, Alors: cacher la balise // Si une balise immage ou une balise vidéo renvoie un message d'erreur, Alors: la cacher
+        // img.setAttribute("onerror", "this.style.display='none'");
+        // vid.setAttribute("onerror", "this.style.display='none'");
+        // test.setAttribute("data-id", id);
+        // jaime.setAttribute("data-jaime", id);
+        // div.classList.add("description-photos");
+        // jaime.classList.add("jaime-photo");
+        // section.classList.add("media");
+        // test.classList.add("likes-img");
+
         // leur attribuer du contenu. Ici objets JSON
-        titre.textContent = title;
-        jaime.textContent = likes;
+        h2_modal.textContent = "Contactez-moi " + name;
+        h1.textContent = name;
+        ville__pays.textContent = city + ", " + country;
+        proverbe.textContent = tagline;
+        // prix_photographe.textContent = price;
+        // titre.textContent = title;
+        // jaime.textContent = likes;
+
         // les placer dans le DOM
-        // document.body.appendChild('section')
-        section.appendChild(img);
-        section.appendChild(titre);
-        section.appendChild(jaime);
-        return (section);
+        // section.appendChild(img);
+        // section.appendChild(vid);
+        // section.appendChild(div);
+        // div.appendChild(titre);
+        // div.appendChild(jaime);
+        // div.appendChild(test);
+
+        // -----------------------
+
+        // Likes
+        // Quand l'image sur laquelle je click a un ID égal à l'ID du paragraphe de la même div, alors j'incrémente de 1 et j'affiche le résultat dans le paragraphe
+        // const pData = document.getElementsByClassName("jaime-photo")
+        // // console.log(pData);
+        // const imgData = document.getElementsByClassName("likes-img")
+        // const likeImgg = data.id;
+    
+        // let compteur = 0;
+        // let array = [];
+ 
+        // -----------------------
+
+        return (medias);
     }
-    return {pictures, getMediaCardDOM }
+    
+    return {pictures, videos, likeImg, portraitHeader, getMediaCardDOM }
 }
 
+// async function Test() {
+//     const likeImgg = document.querySelector("data-likes");
+//     likeImgg.addEventListener("click", function() {
+//         console.log("c'est cliqué !");
+//     })
+// }
+// Test();
 
-// incrémentation pour le compteur likes
-// exemple 1
-// const like = document.getElementById("like");
-// const totalLikes = document.getElementById("likes-total")
-// const likePhoto = document.getElementById("picture")
+
+// // Likes :
+// const likes = document.getElementsByClassName("likes-img");
+// const likePhoto = document.getElementById("picture");
+
 // let compteur = 0;
-//     like.addEventListener("click", function () {
-//         compteur = compteur + 1;
-//         likePhoto.textContent = " Nombre likes première photo " + compteur
-//         totalLikes.innerHTML = " Likes total " + compteur
-
-// });
-
-
-// exemple 2
-const like = document.getElementsByClassName("like");
-const totalLikes = document.getElementById("likes-total")
-const likePhoto = document.getElementById("picture")
-let compteur = 0;
-    for (let i = 0; i < like.length; i++) {
-        like[i].addEventListener("click", function(){
-            compteur ++ ;
-            likePhoto.textContent = compteur
-            totalLikes.innerHTML = " Likes total " + compteur
-        }) 
-    }
-    // for (let i = 0; i < like.length; i++) {
-    //     if(like == "clicked"){
-    //         compteur -- ;
-    //         likePhoto.textContent = compteur
-    //         totalLikes.innerHTML = " Likes total " + compteur
-    //     } 
-    // }
+// let array = Array.from(likes);
+//     for (let i = 0; i < likes.length; i++) {
+//         likes[i].addEventListener("click", function(){
+//             compteur ++ ;
+//             likePhoto.textContent = compteur
+//             array.push(likes[i]);
+//         }) 
+//     }
 
 
-// exemple 3
-// const like = document.getElementById("like");
-// const like2 = document.getElementById("like2");
-// const like3 = document.getElementById("like3");
-// const totalLikes = document.getElementById("likes-total")
-// const likePhoto = document.getElementById("picture")
-// const likePhoto2 = document.getElementById("picture2")
-// const likePhoto3 = document.getElementById("picture3")
-// let compteur = 0;
-// let compteur2 = 0;
-// let compteur3 = 0;
-//     like.addEventListener("click", function () {
-//         let compteurTotal = compteur + compteur2 + compteur3 +1;
-//         compteur ++;
-//         likePhoto.textContent = compteur
-//         totalLikes.innerHTML = compteurTotal
-
-// });
-//     like2.addEventListener("click", function () {
-//         let compteurTotal = compteur + compteur2 + compteur3 +1;
-//         compteur2 ++;
-//         likePhoto2.textContent = compteur2
-//         totalLikes.innerHTML = compteurTotal
-
-// });
-//     like3.addEventListener("click", function () {
-//         let compteurTotal = compteur + compteur2 + compteur3 +1;
-//         compteur3 ++;
-//         likePhoto3.textContent = compteur3
-//         totalLikes.innerHTML = compteurTotal
-
-// });
-
-
-
-// carrousel
+// Carousel
 // au clique sur la modal ouverture/fermeture
 function displayModalPicture() {
     const carrousel = document.getElementById("carrousel");
 	carrousel.style.display = "block";
+    const mediaHide = document.getElementById("liste-medias");
+    mediaHide.style.display = "none";
+    const hide = document.getElementById("carousel-div");
+    hide.style.display = "block";
+    const headerHide = document.getElementById("header-photographer");
+    headerHide.style.display = "none";
+    
 }
 function closeModalPicture() {
     const carrouselClose = document.getElementById("carrousel");
     carrouselClose.style.display = "none";
+    const mediaHide = document.getElementById("liste-medias");
+    mediaHide.style.display = "flex";
+    const hide = document.getElementById("carousel-div");
+    hide.style.display = "none";
+    const headerHide = document.getElementById("header-photographer");
+    headerHide.style.display = "flex";
 }
 // fonctionnement
 document.body.onload=function(){
@@ -127,7 +170,7 @@ document.body.onload=function(){
     for (let i = 0; i <= nbr; i++) {
         div = document.createElement("div");
         div.className = "photo";
-        div.style.backgroundImage = "url('./assets/images/photos/Mimi/im"+i+".jpg')";
+        div.style.backgroundImage = "url('./assets/images/photos/Mimi Keel/im"+i+".jpg')";
         container.appendChild(div);
     }
 }
@@ -144,17 +187,9 @@ g.onclick = function(){
     if (p < 0)
         p++ ;
     
-    container.style.transform = "translate("+ p*  800 + "px)";
+    container.style.transform = "translate("+ p *  800 + "px)";
     container.style.transition = "all O.5s ease";
 }
-
 // n'afficher que les photos des photographes sur lesquels on clique 
 // au clique sur la photo d'un photographe
 // n'afficher que ses médias OU afficher tous les médias et masquer les médias des photographes non concernés
-
-
-
-
-
-
-
