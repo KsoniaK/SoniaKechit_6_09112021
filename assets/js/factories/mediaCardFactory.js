357ab78f124e4981a442c363f1d4ca5fd5cd16e5
sonia kechit <kechits@yahoo.com>
2025-11-18 16:23:17 +0100

    url toutes les images pour gitHub

--- assets/js/factories/mediaCardFactory.js
+++ assets/js/factories/mediaCardFactory.js
@@ -4,8 +4,8 @@
 
 // Création des médias (opérateur (ternaire) conditionnel = 3 opérandes (1 condition, 2 expressions)) et likes pour chaque photographe
 function mediaCardFactory(media) {
-	const photosMedia = `<img src="/assets/images/photos/${photographerFiltres[0].name}/${media.image}" class="images-media" alt="${media.title}"/>`;
-	const videosMedia = `<video src="/assets/images/photos/${photographerFiltres[0].name}/${media.video}" class="videos-media" controls="controls" alt="${media.title}"></video>`;
+	const photosMedia = `<img src="./assets/images/photos/${photographerFiltres[0].name}/${media.image}" class="images-media" alt="${media.title}"/>`;
+	const videosMedia = `<video src="./assets/images/photos/${photographerFiltres[0].name}/${media.video}" class="videos-media" controls="controls" alt="${media.title}"></video>`;
 
 	return `
         <article class="media">
@@ -16,7 +16,7 @@ function mediaCardFactory(media) {
                 <p class="titre-photo">${media.title}</p>
                 <div class="div-jaime">
                     <p class="jaime-photo" data-id="${media.id}">${media.likes}</p>
-                    <img onclick="likes(this)" src="/assets/images/like.png" class="likes-img" data-id="${media.id}" alt="likes"/>
+                    <img onclick="likes(this)" src="./assets/images/like.png" class="likes-img" data-id="${media.id}" alt="likes"/>
                 </div>
             </div>
         </article>`;
